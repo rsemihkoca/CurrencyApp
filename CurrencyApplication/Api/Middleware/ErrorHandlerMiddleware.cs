e5a57762ad52947b96b01854617ef6aa49ff96f5
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-08 01:52:27 +0300

    global error handler

---
+++ CurrencyApplication/Api/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,39 @@
+using System.Net;
+using System.Text.Json;
+using CurrencyApi.Base.Response;
+
+namespace Api.Middleware;
+
+
+public class ErrorHandlerMiddleware(RequestDelegate next)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            var responseModel = new ApiResponse(error.Message);
+
+            switch (error)
+            {
+                case KeyNotFoundException e:
+                    // not found error
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                default:
+                    // unhandled error
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            var result = JsonSerializer.Serialize(responseModel);
+            await response.WriteAsync(result);
+        }
+    }
+}
