b19b465fef7194207639e5d0a475fadd8df917fe
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-09 01:59:59 +0300

    commit
    Signed-off-by: Riza Semih Koca <rsemihkoca@outlook.com>

--- CurrencyApplication/Base/Models/UrlPaths.cs
+++ CurrencyApplication/Base/Models/UrlPaths.cs
@@ -5,9 +5,11 @@ namespace Base.Models;
 public interface IUrlPaths
 {
     string SupportedCurrencies { get; }
+    string ConvertCurrency { get; }
 }
 
 public class UrlPaths : IUrlPaths
 {
-    public string SupportedCurrencies { get; } = "/Symbols";
+    public string SupportedCurrencies { get; } = "/symbols";
+    public string ConvertCurrency { get; } = "/convert?from={0:string}&to={1:string}&amount={2}";
 }
