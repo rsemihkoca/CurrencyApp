a64c666cd3b177680fd146d008bf032996311f50
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-09 01:59:27 +0300

    commit
    Signed-off-by: Riza Semih Koca <rsemihkoca@outlook.com>

--- CurrencyApplication/Business/Cqrs/CurrencyCqrs.cs
+++ CurrencyApplication/Business/Cqrs/CurrencyCqrs.cs
@@ -1,8 +1,11 @@
 using Base.Models;
 using CurrencyApi.Base.Response;
 using MediatR;
+using Schema.Request;
 
 namespace Business.Cqrs;
 
 
 public record GetAllSupportedCurrencies : IRequest<ApiResponse<CurrencyResponse>>;
+
+public record ConvertCurrency(ConvertCurrencyRequest Model) : IRequest<ApiResponse<ConvertCurrencyResponse>>;
