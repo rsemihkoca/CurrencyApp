a2cfe2151edee3f2683655d0d99ca9abdd333ba3
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-09 01:59:40 +0300

    commit
    Signed-off-by: Riza Semih Koca <rsemihkoca@outlook.com>

--- CurrencyApplication/Base/Models/JsonResponses.cs
+++ CurrencyApplication/Base/Models/JsonResponses.cs
@@ -1,7 +1,31 @@
 namespace Base.Models;
 
-public record CurrencyApiResponse
+public record CurrencyJsonResponse
 {
     public bool Success { get; set; }
     public Dictionary<string, string> Symbols { get; set; }
 }
+
+
+public class Info
+{
+    public int timestamp { get; set; }
+    public double rate { get; set; }
+}
+
+public class Query
+{
+    public string from { get; set; }
+    public string to { get; set; }
+    public double amount { get; set; }
+}
+
+public class ConvertCurrencyJsonResponse
+{
+    public bool success { get; set; }
+    public Query query { get; set; }
+    public Info info { get; set; }
+    public string date { get; set; }
+    public double result { get; set; }
+}
+
