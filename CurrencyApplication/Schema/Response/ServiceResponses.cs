335176bbce07e1967c0a1c2b465fb79f5fdd2e32
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-09 01:59:53 +0300

    commit
    Signed-off-by: Riza Semih Koca <rsemihkoca@outlook.com>

---
+++ CurrencyApplication/Schema/Response/ServiceResponses.cs
@@ -0,0 +1,14 @@
+namespace CurrencyApi.Base.Response;
+
+public record CurrencyResponse
+{
+    public Dictionary<string, string> Data { get; set; }
+}
+
+public record ConvertCurrencyResponse
+{
+    public decimal Amount { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public decimal ConvertedAmount { get; set; }
+}
