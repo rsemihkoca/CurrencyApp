a46c17964eb4c2c66d17ebd2c404271b3f6f8eb9
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-11 22:11:27 +0300

    Your commit message for CurrencyApplication/Business/Query/CurrencyQueryHandler.cs

--- CurrencyApplication/Business/Query/CurrencyQueryHandler.cs
+++ CurrencyApplication/Business/Query/CurrencyQueryHandler.cs
@@ -7,7 +7,6 @@ using Business.Cqrs;
 using CurrencyApi.Base.Response;
 using MediatR;
 using Microsoft.Extensions.Options;
-
 using Newtonsoft.Json;
 using RestSharp;
 
@@ -15,7 +14,8 @@ namespace Business.Query;
 
 public class CurrencyQueryHandler :
     IRequestHandler<GetAllSupportedCurrencies, ApiResponse<CurrencyResponse>>,
-    IRequestHandler<ConvertCurrency, ApiResponse<ConvertCurrencyResponse>>
+    IRequestHandler<ConvertCurrency, ApiResponse<ConvertCurrencyResponse>>,
+    IRequestHandler<ConvertMultipleCurrency, ApiResponse<ConvertMultipleCurrencyResponse>>
 {
     private readonly IMapper _mapper;
 
@@ -34,7 +34,7 @@ public class CurrencyQueryHandler :
         CancellationToken cancellationToken)
     {
         var client = new RestClient(_options.Value.Url);
-        
+
         var virtualRequest = new RestRequest(_urlPaths.SupportedCurrencies);
 
         virtualRequest.AddHeader("X-RapidAPI-Key", _options.Value.ApiKey);
@@ -42,7 +42,7 @@ public class CurrencyQueryHandler :
 
         RestResponse<JsonNode> response = await client.ExecuteAsync<JsonNode>(virtualRequest, cancellationToken);
         // var dynamicObject = JsonSerializer.Deserialize<JsonNode>(response.Content);
-        
+
         if (!(bool)response.Data["success"])
         {
             return new ApiResponse<CurrencyResponse>(response.Data["error"]["info"].ToString());
@@ -53,7 +53,7 @@ public class CurrencyQueryHandler :
             CurrencyJsonResponse? apiResponse = JsonConvert.DeserializeObject<CurrencyJsonResponse>(response.Content);
 
             CurrencyResponse currencyData = _mapper.Map<CurrencyResponse>(apiResponse?.Symbols);
-            
+
             return new ApiResponse<CurrencyResponse>(currencyData);
         }
         else
@@ -62,7 +62,8 @@ public class CurrencyQueryHandler :
         }
     }
 
-    public async Task<ApiResponse<ConvertCurrencyResponse>> Handle(ConvertCurrency request, CancellationToken cancellationToken)
+    public async Task<ApiResponse<ConvertCurrencyResponse>> Handle(ConvertCurrency request,
+        CancellationToken cancellationToken)
     {
         // TODO:
         // Convert decimal amount
@@ -70,9 +71,10 @@ public class CurrencyQueryHandler :
         // Rate limiting
         // Validations are working ?
         var client = new RestClient(_options.Value.Url);
-        
-        var requestUrl = String.Format(_urlPaths.ConvertCurrency, request.Model.From, request.Model.To, request.Model.Amount);
-        
+
+        var requestUrl = String.Format(_urlPaths.ConvertCurrency, request.Model.From, request.Model.To,
+            request.Model.Amount);
+
         var virtualRequest = new RestRequest(requestUrl);
 
         virtualRequest.AddHeader("X-RapidAPI-Key", _options.Value.ApiKey);
@@ -87,16 +89,79 @@ public class CurrencyQueryHandler :
 
         if (response.Content != null)
         {
-            ConvertCurrencyJsonResponse? apiResponse = JsonConvert.DeserializeObject<ConvertCurrencyJsonResponse>(response.Content);
-        
+            ConvertCurrencyJsonResponse? apiResponse =
+                JsonConvert.DeserializeObject<ConvertCurrencyJsonResponse>(response.Content);
+
             ConvertCurrencyResponse currencyData = _mapper.Map<ConvertCurrencyResponse>(apiResponse);
-            
+
             return new ApiResponse<ConvertCurrencyResponse>(currencyData);
         }
         else
         {
             return new ApiResponse<ConvertCurrencyResponse>("No content");
         }
-    
+    }
+
+    public async Task<ApiResponse<ConvertMultipleCurrencyResponse>> Handle(ConvertMultipleCurrency request,
+        CancellationToken cancellationToken)
+    {
+        //TODO:
+        // From'da tek bir currency yer almalı
+        // to ve from valid currency olmalı
+        // to ve from aynı olmamalı
+        var client = new RestClient(_options.Value.Url);
+
+        var requestUrl = String.Format(_urlPaths.ConvertMultipleCurrency, request.Model.From);
+        var virtualRequest = new RestRequest(requestUrl);
+
+        virtualRequest.AddHeader("X-RapidAPI-Key", _options.Value.ApiKey);
+        virtualRequest.AddHeader("X-RapidAPI-Host", _options.Value.Host);
+
+        RestResponse<JsonNode> response = await client.ExecuteAsync<JsonNode>(virtualRequest, cancellationToken);
+
+        if (!(bool)response.Data["success"])
+        {
+            return new ApiResponse<ConvertMultipleCurrencyResponse>(response.Data["error"]["info"].ToString());
+        }
+
+        if (response.Content != null)
+        {
+            ConvertMultipleCurrencyJsonResponse? apiResponse =
+                JsonConvert.DeserializeObject<ConvertMultipleCurrencyJsonResponse>(response.Content);
+
+
+            var convertedRates = new Dictionary<string, double>();
+            string fromCurrencyCode = request.Model.From.ToUpper();
+
+            double fromCurrencyRate = apiResponse.Rates.TryGetValue(fromCurrencyCode, out double fromRate)
+                ? fromRate
+                : 0;
+
+            if (fromCurrencyRate == 0)
+            {
+                return new ApiResponse<ConvertMultipleCurrencyResponse>("Target currency rate is 0 or not found");
+            }
+
+
+            foreach (var rate in request.Model.To)
+            {
+                double toCurrencyRate = apiResponse.Rates.TryGetValue(rate.ToUpper(), out double toRate)
+                    ? toRate
+                    : 0;
+
+                double convertedRate = toCurrencyRate / fromCurrencyRate;
+
+                convertedRates.Add(rate.ToUpper(), convertedRate);
+            }
+
+            ConvertMultipleCurrencyResponse currencyData = _mapper.Map<ConvertMultipleCurrencyResponse>(convertedRates);
+
+            return new ApiResponse<ConvertMultipleCurrencyResponse>(currencyData);
+            // return new ApiResponse<ConvertMultipleCurrencyResponse>(new ConvertMultipleCurrencyResponse());
+        }
+        else
+        {
+            return new ApiResponse<ConvertMultipleCurrencyResponse>("No content");
+        }
     }
 }
