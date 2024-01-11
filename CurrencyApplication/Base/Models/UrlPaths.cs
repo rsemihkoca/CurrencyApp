081d1977fd6bed1361e209417a4802a92646e125
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-11 22:11:26 +0300

    Your commit message for CurrencyApplication/Base/Models/UrlPaths.cs

--- CurrencyApplication/Base/Models/UrlPaths.cs
+++ CurrencyApplication/Base/Models/UrlPaths.cs
@@ -6,10 +6,13 @@ public interface IUrlPaths
 {
     string SupportedCurrencies { get; }
     string ConvertCurrency { get; }
+    string ConvertMultipleCurrency { get; }
 }
 
 public class UrlPaths : IUrlPaths
 {
     public string SupportedCurrencies { get; } = "/symbols";
     public string ConvertCurrency { get; } = "/convert?from={0:string}&to={1:string}&amount={2}";
+    
+    public string ConvertMultipleCurrency { get; } = "/latest";
 }
