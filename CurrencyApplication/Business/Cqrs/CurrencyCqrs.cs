5e139a4e1b80f11d08e6e6f988602be6bd28c465
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-11 22:11:26 +0300

    Your commit message for CurrencyApplication/Business/Cqrs/CurrencyCqrs.cs

--- CurrencyApplication/Business/Cqrs/CurrencyCqrs.cs
+++ CurrencyApplication/Business/Cqrs/CurrencyCqrs.cs
@@ -9,3 +9,7 @@ namespace Business.Cqrs;
 public record GetAllSupportedCurrencies : IRequest<ApiResponse<CurrencyResponse>>;
 
 public record ConvertCurrency(ConvertCurrencyRequest Model) : IRequest<ApiResponse<ConvertCurrencyResponse>>;
+
+public record ConvertMultipleCurrency(ConvertMultipleCurrencyRequest Model) : IRequest<ApiResponse<ConvertMultipleCurrencyResponse>>;
+//default olarak from eur. to kısmı boş ise hepsini dönsün.
+
