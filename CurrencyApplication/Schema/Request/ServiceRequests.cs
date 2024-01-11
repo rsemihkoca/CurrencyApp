819ccb95aac56b3a8e481dd54f573b26644353bb
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-11 22:11:27 +0300

    Your commit message for CurrencyApplication/Schema/Request/ServiceRequests.cs

--- CurrencyApplication/Schema/Request/ServiceRequests.cs
+++ CurrencyApplication/Schema/Request/ServiceRequests.cs
@@ -1,3 +1,5 @@
 namespace Schema.Request;
-
+using Base.Models;
 public record ConvertCurrencyRequest(string From, string To, double Amount);
+
+public record ConvertMultipleCurrencyRequest(List<string> To, string From);
